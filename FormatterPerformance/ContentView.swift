c8d7c47ee0f7820a9a8e01d9d3c8443d9f928dc7
Quinn McHenry <quinn@jqm.us>
2021-07-07 15:03:04 -0400

    Initial Commit

---
+++ FormatterPerformance/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FormatterPerformance
+//
+//  Created by Quinn McHenry on 7/7/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FormatterPerformance/FormatterPerformanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormatterPerformanceApp.swift
+//  FormatterPerformance
+//
+//  Created by Quinn McHenry on 7/7/21.
+//
+
+import SwiftUI
+
+@main
+struct FormatterPerformanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
